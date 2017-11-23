3890e87f46672bc09a8ba3786ea9bc7e2d89c151
boscoybarra <bosco@ybarra.io>
2017-11-23 11:16:29 +0100

    Fixed charts getting ready for mainView

--- MyFinance/Controller/CheckerViewController.swift
+++ MyFinance/Controller/CheckerViewController.swift
@@ -8,17 +8,14 @@
 
 import UIKit
 import Charts
-import ChartsRealm
 
 class CheckerViewController: UIViewController {
-    
 
-    @IBOutlet weak var pieVIew: PieChartView!
-    
+    @IBOutlet weak var barChartView: BarChartView!
     
     // Chart data
-    var workoutDuration = [String]()
-    var beatsPerMinute = [String]()
+    var workoutDuration: [String]!
+    //var beatsPerMinute = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,50 +36,35 @@ class CheckerViewController: UIViewController {
     // Populate data
     func populateChartData() {
         workoutDuration = ["1","2","3","4","5","6","7","8","9","10"]
-        beatsPerMinute = ["76","150","160","180","195","195","180","164","136","112"]
-        self.getChartData(with: workoutDuration, values: beatsPerMinute)
+        let beatsPerMinute = [76.0, 150.0, 160.0, 180.0, 195.0, 195.0, 180.0, 164.0, 136.0, 112.0]
+        self.getChartData(dataPoints: workoutDuration, values: beatsPerMinute)
     }
     
-    // Chart options
-    
-//    func cubicChart() {
-//        let pieView = CubicChart(frame: CGRect(x: 0.0, y: 0.0, width: self.view.frame.width, height: self.view.frame.height))
-//        cubicChart.delegate = self as! GetChartData
-//        self.view.addSubview(cubicChart)
-//    }
+
     
     // Conform to protocol
-    func getChartData(with dataPoints: [String], values: [String]) {
-        self.workoutDuration = dataPoints
-        self.beatsPerMinute = values
+    func getChartData(dataPoints: [String], values: [Double]) {
+        
+        barChartView.noDataText = "You need to provide data for the chart"
+        
+        var dataEntries: [BarChartDataEntry] = []
+        var counter = 0.0
+        
+        for i in 0..<dataPoints.count {
+            counter += 1.0
+            let dataEntry = BarChartDataEntry(x: values[i], y: counter)
+            dataEntries.append(dataEntry)
+        }
+        
+        let chartDataSet = BarChartDataSet(values: dataEntries, label: "Time")
+        let chartData = BarChartData()
+        chartData.addDataSet(chartDataSet)
+        barChartView.data = chartData
+        chartDataSet.colors = ChartColorTemplates.colorful()
+        
+        barChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)
+ 
     }
 }
-
-// MARK: - ChartFormatter required to config x axis
-
-
-
-//public class ChartFormatter: NSObject, IAxisValueFormatter {
-//
-//    var workoutDuration = [String]()
-//
-//    public func stringForValue(_ value: Double, axis: AxisBase?) -> String {
-//        return workoutDuration[Int(value)]
-//    }
-//
-//    public func setValues(values:[String]) {
-//        self.workoutDuration = values
-//    }
-//}
-
-//    @IBAction func addValues(_ sender: Any) {
-//        // Make sure there is an inputTable
-//        guard let inputTable = piewView.inputTable else {
-//            return
-//        }
-//        // We create an array of values
-//        let values: [Double] = inputTable.rows.map {_ in return Double(arc4random_uniform(100))}
-//        inputTable.addColumn("col #\(inputTable.columnNames.count + 1)", rowValues: values)
-//    }
     
 
